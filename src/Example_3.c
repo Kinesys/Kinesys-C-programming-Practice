41ff3d9a83af5e408da5d2445d523745f9162c31
Kinesys <67867993+Kinesys@users.noreply.github.com>
2021-03-13 18:12:23 +0900

    Update Example_3.c

--- src/Example_3.c
+++ src/Example_3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-	printf("2 + 5 = %d", 2 + 5);
-	printf("2 - 3 = %d", 2 -3);
-	printf("2 * 3 = %d", 2 * 3);
-	printf("2 / 3 = %d", 2 / 3);
+	printf("2 + 5 = %d\n", 2 + 5);
+	printf("2 - 3 = %d\n", 2 -3);
+	printf("2 * 3 = %d\n", 2 * 3);
+	printf("2 / 3 = %d\n", 2 / 3);
 }
