b9d106c5da6cc73891b4aca276497253778403ec
Kinesys <67867993+Kinesys@users.noreply.github.com>
2021-03-14 00:11:02 +0900

    Create Example_6.c

---
+++ src/Example_6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	printf("5 x 1 = %d\n", 5 * 1);
+	printf("5 x 2 = %d\n", 5 * 2);
+	printf("5 x 3 = %d\n", 5 * 3);
+	printf("5 x 4 = %d\n", 5 * 4);
+	printf("5 x 5 = %d\n", 5 * 5);
+	printf("5 x 6 = %d\n", 5 * 6);
+	printf("5 x 7 = %d\n", 5 * 7);
+	printf("5 x 8 = %d\n", 5 * 8);
+	printf("5 x 9 = %d\n", 5 * 9);
+	return 0;
+}
