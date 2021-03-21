f2cf21e5e4bcf3fe847fff26299655a7b7a96e5c
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-03-22 04:54:32 +0900

    Create 알파벳 문자 아스키 코드 변환.c

---
+++ src/알파벳 문자 아스키 코드 변환.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("소문자 a의 ASCII값은 %d이다\n", 'a');
+  printf("소문자 b의 ASCII값은 %d이다\n", 'b');
+  printf("대문자 A의 ASCII값은 %d이디\n", 'A');
+  printf("대문자 B의 ASCII값은 %d이다\n", 'B');
+
+  printf("ASCII 97은 %c이다 \n", 97);
+  printf("ASCII 98은 %c이다", 98);
+  printf("ASCII 65는 %c이다", 65);
+  printf("ASCII 66은 %c이다", 66);
+
+  return 0;
+}
