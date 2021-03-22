404b83d9736cc57b3de70f41308cfbb0f0d43d5b
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-03-23 04:45:30 +0900

    Create 사칙 연산.c

---
+++ src/사칙 연산.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int x = 20;
+  int y = 10;
+
+  printf("두수의 합 x + y = %d\n", x + y);
+  printf("두수의 차 x - y = %d\n", x - y);
+  printf("두수의 곱 x * y = %d\n", x * y);
+  printf("두수의 몫 x / y = %d\n", x / y);
+
+  return 0;
+}
