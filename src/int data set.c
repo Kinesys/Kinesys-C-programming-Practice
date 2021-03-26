735bce3fd364d3ae45874dc0b937215f1b0c5a6c
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-03-27 05:18:08 +0900

    Create int data set.c

---
+++ src/int data set.c
@@ -0,0 +1,14 @@
+//정수형 변수 number를 선언한 후 초깃값으로 10을 할당하고, 정수형 상수 count를 선언한 후 초깃값으로 5를 할당하는 코드
+#include <stdio.h>
+
+int main() {
+  int number = 10;
+  const int count = 5; //const를 사용해 상수를 선언한다.
+
+
+  printf("number의 값은 %d입니다.\n", %d);
+  printf("count의 값은 %d입니다."\n, %d);
+
+  return 0;
+
+}
