b9292f744bb875049975427a212b3cd9d8c3add4
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-12 18:40:10 +0900

    Create 변수 연산 -1.c

---
+++ src/변수 연산 -1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+	int x = 5, y = 3;
+
+	 int z = 3;
+
+	x = x + 5;
+
+
+	x = y + 3;
+
+	z = z + 4;
+
+	printf("%d\n", x);
+	printf("%d\n", y);
+	printf("%d\n", z);
+
+
+}
