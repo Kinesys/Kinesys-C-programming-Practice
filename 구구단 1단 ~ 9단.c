f3edb19a45d310a1c5e0f9ad03bf745b179b9ea4
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-26 04:38:50 +0900

    Create 구구단 1단 ~ 9단.c

---
+++ 구구단 1단 ~ 9단.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+int main(void) {
+    int one = 1;
+    int two = 2;
+    int three = 3;
+    int four = 4;
+    int five = 5;
+    int six = 6;
+    int seven = 7;
+    int eight = 8;
+    int nine = 9;
+    int ten = 10;
+    int a = 1;
+    int b = 1;
+    int c = 1;
+    int d = 1;
+    int e = 1;
+    int f = 1;
+    int g = 1;
+    int h = 1;
+    int i = 1;
+    
+
+    printf("구구단을 출력합니다.\n");
+    
+    printf("\n");
+    
+  while (a <= 9) { //1단
+    printf("%d * %d = %d\n", one, a, one * a);
+    a++;
+  }
+    printf("\n");
+  while (b <= 9) { //2단
+    printf("%d * %d = %d\n", two, b, two * b);
+    b++;
+  }
+    printf("\n");
+  while (c <= 9) { //3단
+    printf("%d * %d = %d\n", three, c, three * c);
+    c++;
+  }
+    printf("\n");
+  while (d <= 9) { //4단
+    printf("%d * %d = %d\n", four, d, four * d);
+    d++;
+  }
+    printf("\n");
+  while (e <= 9) { //5단
+    printf("%d * %d = %d\n", five, e, five * e);
+    e++;
+  }
+    printf("\n");
+  while (f <= 9) { //6단
+    printf("%d * %d = %d\n", six, f, six * f);
+    f++;
+  }
+    printf("\n");
+  while (g <= 9) { //7단
+    printf("%d * %d = %d\n", seven, g, seven * g);
+    g++;
+  }
+    printf("\n");
+  while (h <= 9) { //8단
+    printf("%d * %d = %d\n", eight, h, eight * h);
+    h++;
+  }
+    printf("\n");
+  while (i <= 9) { //9단
+    printf("%d * %d = %d\n", nine, i, nine * i);
+    i++;
+  }
+    printf("\n");
+
+    return 0;
+}
