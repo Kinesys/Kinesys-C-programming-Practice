f5b869f65b5c7940d179b59f2490485fba9496e9
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-06-16 01:52:24 +0900

    Create for문 예제.c

---
+++ for문 예제.c
@@ -0,0 +1,15 @@
+//for 반복문을 이용해서 동일한 문장 출력하기
+#include <stdio.h>
+
+int main(void) {
+    int i;
+
+    for(i = 0; i < 5; i++) {
+    
+       printf("************\n");
+    
+    }
+
+    return 0;
+
+}
