46eec996baa4f61441efb706ac82edd0a6aa36cf
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-06-18 06:36:07 +0900

    Create 1부터 100까지의 소수의 합.c

---
+++ 1부터 100까지의 소수의 합.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int i, j;
+	int result = 0;
+
+	for (i = 2; i < 100; i++) {
+		for (j = 2; j < i; j++) {
+			if (i % j == 0) break;
+		}
+		if (j == i)
+		result += j;
+	}
+	printf("1부터 100까지의 소수의 합 : %d\n", result);
+
+	return 0;
+}
